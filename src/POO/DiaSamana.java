15cd7660cbd9b5b8b90d23a7fcbf94f3662e61d9
fenoncio123 <fenoncio123@DESKTOP-8N2H851>
2024-02-22 14:18:26 -0600

    Todso los programas

---
+++ src/POO/DiaSamana.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package POO;
+   import java.util.Scanner;
+/** Este progrema dado un numero ingresado te dice que dia de la semana es y si es lavoral o no.
+ *  29/11/23
+ * @author Manuel Alejandro Gómez Molina.
+ */
+public class DiaSamana {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    int a[]=new int[4];
+        a[0]=2;
+        a[1]=2;
+        a[2]=2;
+        a[3]=2;
+        a[4]=2;
+        }
+    }
